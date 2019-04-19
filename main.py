d454477bc1d656064e9bbf102b508f3075135161
ipruett <ian.pruett@wsu.edu>
2019-04-18 20:34:53 -0700

    multi-step prediction works!!

--- main.py
+++ main.py
@@ -8,20 +8,10 @@ from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.layers import LSTM, TimeDistributed
 
 
-# define base directory
+# define base directory this changes 
+# when running in Google CoLab 
 base_dir = ''
 
-
-def load_dataframe(stock):
-    # read file into dataframe
-    filepath = base_dir + 'data/processed/'+ stock + '.open.txt'
-    dataframe = pd.read_csv(
-        filepath,
-        usecols=[1], 
-        engine='python',
-        skipfooter=3
-    )
-    return dataframe
 
 def load_dataset(stock):
     filepath = base_dir + 'data/Stocks/' + stock + '.us.txt'
@@ -31,9 +21,9 @@ def load_dataset(stock):
     return data
 
 
-# look_back is the number of times steps to look back
-# where as look_ahead is the number of times steps
-# to predict
+# indexes time-series data into features and 
+# corresponding output. For each vector x in X
+# there is a corresponding y
 def process_dataset(D,look_back=1,look_ahead=1):
     
     # scale data between 0,1
@@ -50,12 +40,20 @@ def process_dataset(D,look_back=1,look_ahead=1):
         k = j + look_ahead
         X_entry = D[i:j,0]
         y_entry = D[j:k,0]
+        if len(X_entry) != look_back:   # alternative to fixing
+            break                       # index errors, got lazy . . .
+        if len(y_entry) != look_ahead:
+            break
         X.append(X_entry)
         y.append(y_entry)
 
     return np.array(X),np.array(y)
 
 
+
+# Splits dataset into train and test given 
+# given features X, output y, and the 
+# size of test sample
 def split_train_test(X,y,test_size=.33):
     t = int(len(X) * (1-test_size))
     X_train = X[0:t]
@@ -65,10 +63,12 @@ def split_train_test(X,y,test_size=.33):
     return X_train,X_test,y_train,y_test
 
 
+# Splits dataset into train and test given 
+# given original dataset size of test sample
 def split_dataset_train_test(D,test_size=.33):
     t = int(len(D) * (1-test_size))
-    D_train = D[0:t]
-    D_test = D[t:len(D)]
+    D_test = D[0:t]
+    D_train = D[t:len(D)]
     return D_train,D_test
 
 
@@ -95,14 +95,10 @@ def time_series_predict(model,X,k=1):
                 d[(p-i):(p)] = queue[0:i]
             # make prediction and add to queue
             d = np.expand_dims(d,axis=1)
-            d = np.expand_dims(d,axis=2)
-            print(d.shape)
-            thing = model.predict([d])
-            print(thing.shape)
-            print(thing)
-            queue[i] = model.predict([d])
+            d = np.array([d])
+            queue[i] = model.predict([d]).flatten()
 
-        return queue
+        return queue # set of predictions of k steps
 
     return np.apply_along_axis(predict_foward_steps,1,X)            
 
@@ -118,6 +114,7 @@ def simple_lstm_network(time_steps):
         recurrent_activation='sigmoid',
         unroll=False,
         use_bias=True,
+        name='sirst'
     ))
     
     model.add(Dropout(0.2))
@@ -130,20 +127,19 @@ def simple_lstm_network(time_steps):
         recurrent_activation='sigmoid',
         unroll=False,
         use_bias=True,
+        name='second'
     ))
     
     model.add(Dropout(0.2))
 
     # output layer
-    # model.add(TimeDistributed(Dense(predict_steps)))
     model.add((Dense(1)))
-    
 
     model.compile(
         loss='mse',
         optimizer='adam',
-        # metrics=['accuracy','mae']
-        metrics=['accuracy']
+        metrics=['accuracy','mae']
+        # metrics=['accuracy']
     )
 
     return model
@@ -155,11 +151,13 @@ def main():
     D = D[:,1].astype(float)
     D = np.expand_dims(D,axis=2)
 
+    # define time steps, prev and foward
     time_steps = 30
-    predict_steps = 7
+    predict_steps = 10
 
     D_test,D_train = split_dataset_train_test(D)
 
+    # index training and test sets
     X_train,y_train = process_dataset(
         D_train,
         look_back=time_steps,
@@ -172,14 +170,9 @@ def main():
         look_ahead=predict_steps
     )
 
+    # reshape to train lstm network
     X_train = np.expand_dims(X_train,axis=2)
     X_test = np.expand_dims(X_test,axis=2)
-    # y = np.expand_dims(y,axis=1)
-
-    # X_train,X_test,y_train,y_test = split_train_test(
-    #     X,y,
-    #     test_size=.33
-    # )
 
     model = simple_lstm_network(
         time_steps=time_steps
@@ -191,18 +184,22 @@ def main():
         epochs=5
     )
 
+    # model.save('models/my_baby.model')
+
+    # model = tf.keras.models.load_model('models/my_baby.model')
+
     # predictions = model.predict([X_test])
     predictions = time_series_predict(
-        model,X_train,
-        k=predict_steps
+        model,X_test,k=predict_steps
     )
 
     # loss, acc, mae = model.evaluate(X_test,y_test)
     # print('Loss: %f\tAcc: %f\tMAE: %f' % (loss,acc,mae))
-    loss, acc = model.evaluate(X_test,y_test)
-    print('Loss: %f\tAcc: %f' % (loss,acc))
+    # loss, acc = model.evaluate(X_test,y_test)
+    # print('Loss: %f\tAcc: %f' % (loss,acc))
 
-    plt.plot(y_test.flatten())
+    ground_truth = y_test.flatten()
+    plt.plot(ground_truth)
     plt.plot(predictions.flatten())
     plt.show()
    
